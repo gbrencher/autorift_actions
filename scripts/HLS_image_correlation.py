ed56085542cae615e9034364dd221ceed559a7af
gbrencher <gbrench@uw.edu>
2025-11-04 09:03:44 -0800

    updating gdal env variables

--- scripts/HLS_image_correlation.py
+++ scripts/HLS_image_correlation.py
@@ -20,6 +20,12 @@ import numpy as np
 warnings.filterwarnings("ignore", category=RuntimeWarning)
 warnings.filterwarnings("ignore", category=UserWarning)
 
+gdal.SetConfigOption('GDAL_HTTP_COOKIEFILE','~/cookies.txt')
+gdal.SetConfigOption('GDAL_HTTP_COOKIEJAR', '~/cookies.txt')
+gdal.SetConfigOption('GDAL_DISABLE_READDIR_ON_OPEN','EMPTY_DIR')
+gdal.SetConfigOption('CPL_VSIL_CURL_ALLOWED_EXTENSIONS','TIF')
+gdal.SetConfigOption('GDAL_HTTP_UNSAFESSL', 'YES')
+
 def download_hls(img1_date, img2_date, aoi):
     '''
     Download a pair of HLS images acquired on given dates over a given area of interest
